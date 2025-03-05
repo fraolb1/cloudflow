95fcb5443cace14572dc3fe2e644043302d9e558
Firaol Bogale <firaolbogala98@gmail.com>
2025-03-05 15:39:26 +0300

    added components to show recent files and show storage space used

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -18,20 +18,19 @@ const Dashboard = async () => {
 
   // Get usage summary
   const usageSummary = getUsageSummary(totalSpace);
-  console.log(files);
 
   return (
-    <div className="dashboard-container">
+    <div className="mx-auto grid max-w-7xl grid-cols-1 gap-6 md:grid-cols-2 xl:gap-10">
       <section>
         <Chart used={1.2} />
 
         {/* Uploaded file type summaries */}
-        <ul className="dashboard-summary-list">
+        <ul className="mt-6 grid grid-cols-1 gap-4 xl:mt-10 xl:grid-cols-2 xl:gap-9">
           {usageSummary.map((summary) => (
             <Link
               href={summary.url}
               key={summary.title}
-              className="dashboard-summary-card"
+              className="relative mt-6 rounded-[20px] bg-white p-5 transition-all hover:scale-105"
             >
               <div className="space-y-4">
                 <div className="flex justify-between gap-3">
@@ -40,14 +39,16 @@ const Dashboard = async () => {
                     width={100}
                     height={100}
                     alt="uploaded image"
-                    className="summary-type-icon"
+                    className="absolute -left-3 top-[-25px] z-10 w-[190px] object-contain"
                   />
-                  <h4 className="summary-type-size">
+                  <h4 className="h4 relative z-20 w-full text-right">
                     {convertFileSize(1.2) || 0}
                   </h4>
                 </div>
 
-                <h5 className="summary-type-title">{summary.title}</h5>
+                <h5 className="h5 relative z-20 text-center">
+                  {summary.title}
+                </h5>
                 <Separator className="bg-light-400" />
                 <FormattedDateTime
                   date={summary.latestDate}
@@ -60,7 +61,7 @@ const Dashboard = async () => {
       </section>
 
       {/* Recent files uploaded */}
-      <section className="dashboard-recent-files">
+      <section className="h-full rounded-[20px] bg-white p-5 xl:p-8">
         <h2 className="h3 xl:h2 text-light-100">Recent files uploaded</h2>
         {files.documents.length > 0 ? (
           <ul className="mt-5 flex flex-col gap-5">
@@ -77,9 +78,11 @@ const Dashboard = async () => {
                   url={file.url}
                 />
 
-                <div className="recent-file-details">
+                <div className="flex w-full flex-col xl:flex-row xl:justify-between">
                   <div className="flex flex-col gap-1">
-                    <p className="recent-file-name">{file.name}</p>
+                    <p className="flex w-full flex-col xl:flex-row xl:justify-between">
+                      {file.name}
+                    </p>
                     <FormattedDateTime
                       date={file.createdAt}
                       className="caption"
@@ -91,7 +94,9 @@ const Dashboard = async () => {
             ))}
           </ul>
         ) : (
-          <p className="empty-list">No files uploaded</p>
+          <p className="body-1 mt-10 text-center text-light-200">
+            No files uploaded
+          </p>
         )}
       </section>
     </div>
